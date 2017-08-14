cc3bc6c59da91c54ef878b87b819d39a66fce9fc
jasonelchen <jasonelchen@tencent.com>
2017-08-14 22:16:31 +0800

    修改基础配置信息

--- client/config.js
+++ client/config.js
@@ -3,8 +3,7 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-// var host = 'dev-5.wxtool.f-xj.org/weapp';
-var host = 'localhost:5757';
+var host = 'abcdedg.qcloud.la';
 
 var config = {
 
@@ -13,16 +12,16 @@ var config = {
         host,
 
         // 登录地址，用于建立会话
-        loginUrl: `https://${host}/login`,
+        loginUrl: `https://${host}/weapp/login`,
 
         // 测试的请求地址，用于测试会话
-        requestUrl: `https://${host}/user`,
+        requestUrl: `https://${host}/weapp/user`,
 
         // 测试的信道服务地址
-        tunnelUrl: `https://${host}/tunnel`,
+        tunnelUrl: `https://${host}/weapp/tunnel`,
 
         // 上传图片接口
-        uploadUrl: `https://${host}/upload`
+        uploadUrl: `https://${host}/weapp/upload`
     }
 };
 

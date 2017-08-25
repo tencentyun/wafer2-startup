36f408aad6dbbac4e40784371810f7aefa1689f8
jasonelchen <jasonelchen@tencent.com>
2017-08-25 10:23:59 +0800

    修改 config.js

--- client/config.js
+++ client/config.js
@@ -3,7 +3,7 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = 'abcefghi.qcloud.la';
+var host = 'https://abcefghi.qcloud.la';
 
 var config = {
 
@@ -12,16 +12,16 @@ var config = {
         host,
 
         // 登录地址，用于建立会话
-        loginUrl: `https://${host}/weapp/login`,
+        loginUrl: `${host}/weapp/login`,
 
         // 测试的请求地址，用于测试会话
-        requestUrl: `https://${host}/weapp/user`,
+        requestUrl: `${host}/weapp/user`,
 
         // 测试的信道服务地址
-        tunnelUrl: `https://${host}/weapp/tunnel`,
+        tunnelUrl: `${host}/weapp/tunnel`,
 
         // 上传图片接口
-        uploadUrl: `https://${host}/weapp/upload`
+        uploadUrl: `${host}/weapp/upload`
     }
 };
 
